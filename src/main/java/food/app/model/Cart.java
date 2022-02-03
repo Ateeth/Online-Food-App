07085331db3b0f8e3af6e106557a2011442884f7
Ateeth Arun <58625369+Ateeth@users.noreply.github.com>
2022-02-03 11:24:56 +0400

    Create Cart.java

---
+++ src/main/java/food/app/model/Cart.java
@@ -0,0 +1,17 @@
+package food.app.model;
+
+public class Cart extends Food{
+	private int quantity ; 
+	
+	public Cart() {}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
